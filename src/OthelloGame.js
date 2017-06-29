a7de58fa8f82876b1c16eda70173a4d27b86d317
Eliot Beresford <eliotberesfordeliotberesford@outlook.com>
2017-06-29 15:10:06 +0100

    Store game state within top-level component

--- src/OthelloGame.js
+++ src/OthelloGame.js
@@ -3,20 +3,25 @@ import './OthelloGame.css';
 import Board from './Board';
 import {W, B, E, getAnnotatedBoard} from './game-logic';
 
-const board = [
-  [E, E, E, E, E, E],
-  [E, E, E, E, E, E],
-  [E, E, W, B, E, E],
-  [E, E, B, W, E, E],
-  [E, E, E, E, E, E],
-  [E, E, E, E, E, E]
-];
-
 class OthelloGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [
+        [E, E, E, E, E, E],
+        [E, E, E, E, E, E],
+        [E, E, W, B, E, E],
+        [E, E, B, W, E, E],
+        [E, E, E, E, E, E],
+        [E, E, E, E, E, E]
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="OthelloGame">
-        <Board board={getAnnotatedBoard(board)}/>
+        <Board board={getAnnotatedBoard(this.state.board)}/>
       </div>
     );
   }
